dc8625410266d40ebd926b3402d8591ac78ebed4
GunterMagno <bcalejandro@iesrafaelalberti.es>
2025-01-23 21:19:10 +0100

    aceitunass

---
+++ src/Domicilio.kt
@@ -0,0 +1,5 @@
+class Domicilio(private val calle :String,private val numero :Int) {
+
+    fun dirCompleta(){"Especificaciones domicilio:\n\tCalle: $calle\n\tNumero: $numero"}
+
+}
---
+++ src/RepositorioCompras.kt
@@ -0,0 +1,18 @@
+class RepositorioCompras(val compras :MutableList<Compra>) {
+
+    fun agregarCompra(compra: Compra){compras.add(compra)}
+
+    fun agregarCompra(cliente: Cliente,dia :Int,total:Double){compras.add(Compra(cliente,dia,total))}
+
+    fun agregarCompra(nombre :String, calle :String, numero :Int, dia :Int, total :Double)
+    {compras.add(Compra(Cliente(nombre,Domicilio(calle,numero)),dia,total))}
+
+    fun domicilios(): List<Domicilio>{
+        val conjuntoDomicilio = mutableSetOf<Domicilio>()
+        for (compra in compras){conjuntoDomicilio.add(compra.cliente.domicilio)}
+        return conjuntoDomicilio.toList()
+    }
+
+    fun domiciliosV1() = compras.map { it.cliente.domicilio }.toSet().toList()
+
+}
