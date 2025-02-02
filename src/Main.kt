67ffc046941b5bceda7ec883dc570a5c9467d70e
GunterMagno <aborcru560@g.educaand.es>
2025-02-02 21:12:50 +0100

    terminadou (se me olvido el commit)

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,14 @@
 
-fun main(){
+
+fun main() {
     val repositorioCompras = RepositorioCompras()
+
+    val cliente1 = Cliente("Juan Pérez", Domicilio("Av. Patatas con mayo", 123))
+    val cliente2 = Cliente("María Gómez", Domicilio("Calle alonso", 456))
+
+    repositorioCompras.agregarCompra(cliente1, 5, 150.0)
+    repositorioCompras.agregarCompra(cliente2, 10, 200.0)
+
+    println("Domicilios únicos de clientes:")
+    repositorioCompras.domicilios().forEach { println(it.dirCompleta()) }
 }
